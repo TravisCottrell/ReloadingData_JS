65c82ab3900f89964a452604879aed6f5a59c490
TravisCottrell <tcottre4@kent.edu>
2022-10-31 18:27:10 -0400

    added build to backend

--- back-end/app.js
+++ back-end/app.js
@@ -10,12 +10,12 @@ const gunRouter = require("./routes/gun");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, "../front-end/build")));
+app.use(express.static(path.resolve(__dirname, "./build")));
 
 app.use("/api/guns", gunsRouter);
 app.use("/api/gun", gunRouter);
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../front-end/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./build", "index.html"));
 });
 
 const port = process.env.PORT || 3005;
