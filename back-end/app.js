b102f14960c3abd4e38d4240013aa65bc67caef9
TravisCottrell <tcottre4@kent.edu>
2022-10-31 19:08:56 -0400

    updating dev branch

--- back-end/app.js
+++ back-end/app.js
@@ -10,7 +10,6 @@ const gunRouter = require("./routes/gun");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, "./build")));
 
 app.use("/api/guns", gunsRouter);
 app.use("/api/gun", gunRouter);
