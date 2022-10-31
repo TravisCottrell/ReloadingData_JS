7391fc37245d5f6c4eb0eeefce12912fd15489bf
TravisCottrell <tcottre4@kent.edu>
2022-10-31 16:07:55 -0400

    testing

--- back-end/app.js
+++ back-end/app.js
@@ -12,12 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "../front-end/build")));
 
-app.use(express.static(path.resolve(__dirname, "../front-end/build")));
-
 app.use("/api/guns", gunsRouter);
 app.use("/api/gun", gunRouter);
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../front-end/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 const port = process.env.PORT || 3005;
