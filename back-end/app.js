ed69f51818f5ff6296499a516fac990b2224df87
TravisCottrell <tcottre4@kent.edu>
2022-10-31 15:29:01 -0400

    setup for deploy part 1

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -9,9 +10,13 @@ const gunRouter = require("./routes/gun");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "../front-end/build")));
 
 app.use("/api/guns", gunsRouter);
 app.use("/api/gun", gunRouter);
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../front-end/build/index.html"));
+});
 
 const port = process.env.PORT || 3005;
 
